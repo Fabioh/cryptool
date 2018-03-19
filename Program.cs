037c3ec32dfec58f0ec53ee872a4f5b4c5ec8dcd
Vinicius Quaiato <vinicius.quaiato@lambda3.com.br>
2018-03-19 16:12:58 -0300

    creating .NET Core 2.1 global tool for Bitcoin to USD|GBP|EUR|BRL

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cryptool
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static string ArgsResolver(string[] args)
+        {
+            if(args.Any() && Regex.IsMatch(args[0], "^[a-zA-Z]{3}$"))
+                return $"/{args[0]}";
+            
+            return string.Empty;
+        }
+
+        static async Task Main(string[] args)
+        {
+            var uri = $"https://api.coindesk.com/v1/bpi/currentprice{ArgsResolver(args)}.json";
+
+            var result = await client.GetAsync(uri);
+            var content = await result.Content.ReadAsStringAsync();
+
+            if (result.StatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Ocorreu um problema 😬");
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine(content);          
+        }
+    }
+}
